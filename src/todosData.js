5d0b8ff72a856fd8f9071354a47eb6567a0bb721
Fernando Salviati <fernandosalviati@gmail.com>
2020-11-18 11:25:13 +1100

    Add files via upload

---
+++ src/todosData.js
@@ -0,0 +1,27 @@
+const todosData = [{
+    id: 1,
+    text: "Take out the trash",
+    completed: false
+},
+{
+    id: 2,
+    text: "Grocery shopping",
+    completed: false
+},
+{
+    id: 3,
+    text: "Clean the dog",
+    completed: false
+},
+{
+    id: 4,
+    text: "Mow Lawn",
+    completed: false
+},
+{
+    id: 5,
+    text: "Catch up with friends",
+    completed: false
+}]
+
+export default todosData
